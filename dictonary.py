36d028042e6a2d5b9f2d71f766920392b08ecd74
jyothikorrapati123 <144519938+jyothikorrapati123@users.noreply.github.com>
2023-10-19 16:32:33 +0530

    Add files via upload

---
+++ dictonary.py
@@ -0,0 +1,10 @@
+#implicit
+userDetails={'firstname':'jyothi','lastname':'korrapati','email':'jyothikorrapati@gmail.com'}
+print(userDetails)
+#explicit
+userDetails=dict({'firstname':'jyothi','lastname':'korrapati','email':'jyothikorrapati@gmail.com'})
+print(type(userDetails))
+#data type/variable annotation
+userDetails:dict={'firstname':'jyothi','lastname':'korrapati','email':'jyothikorrapati@gmail.com'}
+print(type(userDetails))
+
